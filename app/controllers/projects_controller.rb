db29124616bfed100a2357263c9e720a4ccdee2f
Stanly Liu <sjcliu@gmail.com>
2016-12-14 13:09:17 +0800

    Setup and styled basic project functionality

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,35 @@
+class ProjectsController < ApplicationController
+
+    before_action :find_project, only: [:show, :edit, :update]
+
+    def index
+        @projects = Project.all.order('created_at DESC')
+    end
+
+    def new
+        @project = Project.new
+    end
+
+    def create
+        @project = Project.new project_params
+
+        if @project.save
+            redirect_to @project, notice: "Nice Stan! That project was successfully saved"
+        else
+            render 'new'
+        end
+    end
+
+    def show
+    end
+
+    private
+
+    def find_project
+        @project = Project.find(params[:id])
+    end
+
+    def project_params
+        params.require(:project).permit(:title, :description, :link)
+    end
+end
