74d72a9ba798abad6f6180d159e45ec54620a1b5
Stanly Liu <sjcliu@gmail.com>
2016-12-14 13:21:36 +0800

    Add edit and destroy project abilities

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
 
-    before_action :find_project, only: [:show, :edit, :update]
+    before_action :find_project, only: [:show, :edit, :update, :destroy]
 
     def index
         @projects = Project.all.order('created_at DESC')
@@ -21,6 +21,22 @@ class ProjectsController < ApplicationController
     end
 
     def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @project.update project_params
+            redirect_to @project, notice: "Great project was successfully updated."
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @project.destroy
+        redirect_to projects_path
     end
 
     private
